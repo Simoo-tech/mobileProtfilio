bddef4c8377d83914a00d64eb8f38255666e5af0
salah hamed <school12p@gmail.com>
2023-05-02 01:01:31 +0300

    ASdas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { WebShare } from "./pages/WebShare";
 
 function App() {
   return (
-    <Router basename="/mobileProtfilio">
+    <BrowserRouter basename="/mobileProtfilio">
       <div className="App">
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/webshare" element={<WebShare />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 export default App;
