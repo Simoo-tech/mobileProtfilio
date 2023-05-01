1c0e6e0c2aadf408f2a86800298d507f22f2a72b
salah hamed <school12p@gmail.com>
2023-05-01 23:40:40 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { WebShare } from "./pages/WebShare";
-import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/mobile-protfilio">
         <Routes>
-          <Route path="/mobile-protfilio" exact element={<Home />} />
+          <Route path="/" exact element={<Home />} />
           <Route path="/webshare" element={<WebShare />} />
         </Routes>
       </Router>
